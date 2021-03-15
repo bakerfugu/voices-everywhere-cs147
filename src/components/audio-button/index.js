ddabfdf58f6dcbdc756fd75a9ed8c12398b9571f
Baker Sharp <bakers@stanford.edu>
2021-03-14 22:01:35 -0400

    added audio loading button

--- src/components/audio-button/index.js
+++ src/components/audio-button/index.js
@@ -3,6 +3,7 @@ import useSound from 'use-sound';
 
 import PauseButton from '@site/static/img/pause-button.svg';
 import PlayButton from '@site/static/img/play-button.svg';
+import LoadingAudioButton from '@site/static/img/loading-audio-button.svg';
 import { IntersectionContext } from "../intersection-observer";
 
 import styles from './styles.module.css';
@@ -11,30 +12,41 @@ export function AudioButton({
   audioFilePath,
 }) {
   const { inView } = useContext(IntersectionContext);
-  const [play, { pause, stop }] = useSound(audioFilePath);
-  const [playing, setPlaying] = useState(false);
+  const [play, { pause, stop, isPlaying, duration }] = useSound(audioFilePath);
 
   useEffect(() => {
-    if (!inView) stop();
-    return () => stop();
+    if (!inView) {
+      // console.log('Audio stopping cuz inview false.', duration, !!stop);
+      stop();
+    }
+    return () => {
+      // console.log('Audio stopping from inView unmount', duration, !!stop);
+      stop();
+    }
   }, [inView])
+
+  useEffect(() => {
+    return () => {
+      // console.log('Audio stopping from full compon unmount', duration, !!stop);
+      stop();
+    }
+  }, [])
   
+  // console.log('Audio button. inView?', inView, 'isPlay', isPlaying, 'durat', duration, 'snip.');
   return (
     <div
       className={styles.clickable}
       onClick={() => {
-        setPlaying(playingUpdated => {
-          if (playingUpdated) {
-            pause();
-            return false;
-          } else {
-            play();
-            return true;
-          }
-        });
+        if (isPlaying) {
+          pause();
+        } else {
+          play();
+        }
       }}
     >
-      { playing ? 
+      { !duration ?
+        <LoadingAudioButton/>
+      : isPlaying ? 
         <PauseButton/>
       :
         <PlayButton/>
