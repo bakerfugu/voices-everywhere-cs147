6db0e9674982176a281dcaaeaa9965d38bd231d9
Baker Sharp <bakers@stanford.edu>
2021-04-20 13:15:51 -0700

    Deploy website - based on 2a6036e2f4f569f07ee3782439b64c996afcd30f

--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-export * from "./parallax-box";
-export * from "./intersection-observer";
-export * from "./scale-box";
-export * from "./fade-box";
-export * from "./feature-bubble-variants";
-export * from "./story-orb";
-export * from "./audio-button";
